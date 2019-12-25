1b693a95b08e758a0586377fd36292c8781db3a7
Ascanio Savio <ascaniosavio97@gmail.com>
2019-12-25 02:21:59 -0300

    Update SimpleSampling.py

--- Statistic I/SimpleSampling.py
+++ Statistic I/SimpleSampling.py
@@ -8,7 +8,7 @@ base = pandas.read_csv('iris.csv') #data base
 #creating a sampling
 #parameters of random.choice(): numbers that you want to randomize in array format, size of sampling, replace, probabillity of each number  
 
-#Use this method when you can keep the sampling result. Note : You can put any value for this.
+#Use this method when you want to keep the sampling result. Note : You can put any value for this.
 #numpy.random.seed(123)
 amostra = numpy.random.choice(a = [0, 1], size = 150, replace = True, p = [0.5, 0.5])
 print(amostra)
